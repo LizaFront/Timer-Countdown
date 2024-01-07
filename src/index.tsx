c0c02413671f16d0c63f315865112dfe5a8c26ed
Elizaveta <lizasteplion@gmail.com>
2024-01-07 18:41:27 +0700

    fixed react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from 'store/store';
@@ -14,12 +14,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <StyledEngineProvider injectFirst>
                     <App />
                     <GlobalStyles />
                 </StyledEngineProvider>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
